07a0899d8a70f43a316246d7cbea10cf58eb5ee8
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2024-03-26 01:44:28 +0530

    third stage

--- app/server.go
+++ app/server.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+func handleconn(conn net.Conn) {
+	pong := "+PONG\r\n"
+	_, err := conn.Write([]byte(pong))
+	if err != nil {
+		fmt.Println("Could not write to the client")
+		os.Exit(1)
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -18,12 +27,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleconn(conn)
 	}
-	pong := "+PONG\r\n"
-	_, err = conn.Write([]byte(pong))
 
 }
