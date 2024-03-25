04105111e382f7924891025df63fb69b31f127c2
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2024-03-26 01:41:29 +0530

    second stage

--- app/server.go
+++ app/server.go
@@ -18,9 +18,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	pong := "+PONG\r\n"
+	_, err = conn.Write([]byte(pong))
+
 }
