05f8faad68a64914f0d589a2f6adeaf164fc0960
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2024-03-31 03:33:53 +0530

    Big changes

--- app/server.go
+++ app/server.go
@@ -2,35 +2,56 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 
-	"github.com/codecrafters-io/redis-starter-go/app/parser"
+	"github.com/codecrafters-io/redis-starter-go/commands"
+	"github.com/codecrafters-io/redis-starter-go/resp"
+	"github.com/codecrafters-io/redis-starter-go/store"
 )
 
 func handleconn(conn net.Conn) {
 	defer conn.Close()
-	// pong := "+PONG\r\n"
-	// conn.Write([]byte(pong))
+
+	fmt.Println("Connected")
+	respHandler := resp.NewRespHandler(conn)
+	respWriter := resp.NewRespWriter(conn)
+	clientDb := store.NewDb()
 	for {
-		buf := make([]byte, 124)
-		_, err := conn.Read(buf)
+		value, err := respHandler.ParseAny()
+		fmt.Println(value)
 		if err != nil {
-			fmt.Println("Could not read the client messagex")
+			fmt.Println("From Here", err)
 		}
-		response := parser.MainParser(buf)
-		conn.Write(response)
+		switch value.Typ {
+		case resp.ArrayType:
+
+			Reqargs := value.Array
+			Comm := Reqargs[0].Bulk
+			// fmt.Println(Comm)
+			Comm_Args := Reqargs[1:]
+			respValue := commands.Handlers[strings.ToLower(Comm)](clientDb, Comm_Args)
+			respWriter.Write(respValue)
+		case resp.StringType:
+			switch strings.ToLower(value.Str) {
+			case "ping":
+				respValue := resp.Value{
+					Typ: resp.StringType,
+					Str: "PONG",
+				}
+
+				respWriter.Write(respValue)
+			}
+
+		}
 	}
 
 }
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -46,3 +67,24 @@ func main() {
 	}
 
 }
+
+// func maintest() {
+// 	fmt.Println("Logs from your program will appear here!")
+
+// 	l, err := net.Listen("tcp", ":4000")
+// 	if err != nil {
+// 		fmt.Println("Failed to bind to port 6379")
+// 		os.Exit(1)
+// 	}
+// 	go test.Test()
+
+// 	for {
+// 		conn, err := l.Accept()
+// 		if err != nil {
+// 			fmt.Println("Error accepting connection: ", err.Error())
+// 			os.Exit(1)
+// 		}
+// 		go handleconn(conn)
+// 	}
+
+// }
